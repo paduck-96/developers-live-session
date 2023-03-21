0e2f0d8431a05aae050ef8798eb7f7db7a4f83e6
lango <xmun777@naver.com>
2023-03-21 16:59:57 +0900

    feat: developers live Session server init

---
+++ src/main/java/com/developers/livesession/developers/DevelopersLiveSessionApplication.java
@@ -0,0 +1,13 @@
+package com.developers.livesession.developers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevelopersLiveSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DevelopersLiveSessionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/developers/livesession/developers/DevelopersLiveSessionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developers.livesession.developers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopersLiveSessionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
