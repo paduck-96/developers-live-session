9164890e254751bf6f4d7db5e284b9389039c772
paduck-96 <peterrabitt1234@naver.com>
2023-03-22 19:28:24 +0900

    feat: live session service api init

---
+++ src/main/java/com/developers/livesession/developers/session/dto/SessionRedisFindAllResponse.java
@@ -0,0 +1,16 @@
+package com.developers.livesession.developers.session.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Data
+public class SessionRedisFindAllResponse {
+    private String code;
+    private String msg;
+    private String data;
+}
---
+++ src/main/java/com/developers/livesession/developers/session/dto/SessionRedisSaveRequest.java
@@ -0,0 +1,17 @@
+package com.developers.livesession.developers.session.dto;
+
+import com.developers.livesession.developers.session.constant.Type;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class SessionRedisSaveRequest {
+    @NotNull
+    private String roomId;
+    @NotNull
+    private Type type;
+}
